80e1125a76e853fd863b440666211a58326c79e4
Priyap1038 <124154881+Priyap1038@users.noreply.github.com>
2024-05-08 16:41:55 +0530

    counter done

---
+++ purecompo/src/Components/SimpleCounter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const SimpleCounter = () => {
+
+    const [state, setstate] = useState({
+        count:0,
+        toggle:false
+    })
+
+    const handleToggel=()=>{
+        
+        setstate({
+            ...state,
+            toggle:state.toggle?false:true
+        })
+    }
+
+    const handleInc=()=>{
+    state.toggle? setstate({...state,count:state.count+1}):setstate({...state,count:state.count})
+    }
+
+
+  return (
+    <div>
+      <h1>Simple Counter</h1>
+      <h1>{state.count}</h1>
+      <button onClick={handleToggel}>Set toggel</button>
+      <button onClick={handleInc}>increase</button>
+    </div>
+  );
+};
+
+export default SimpleCounter;
